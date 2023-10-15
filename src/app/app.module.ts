41829fb6392d30cc5fa6996b24504fc161b4aafa
Yograj010 <yograj0147@gmail.com>
2023-10-15 16:51:12 +0530

    revised component inline style & inline template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserListComponent } from './user-list/user-list.component';
+import { StudentListComponent } from './student-list/student-list.component';
+import { CountryListComponent } from './country-list/country-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserListComponent
+    UserListComponent,
+    StudentListComponent,
+    CountryListComponent
   ],
   imports: [
     BrowserModule,
--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-user-list',
-  // selector: 'app-user-line',       //selector name can be changed like done here
   templateUrl: './user-list.component.html',
-  styleUrls: ['./user-list.component.css']
+  styles: [
+    `.custom{color:red}`
+  ]
 })
 export class UserListComponent {
 
